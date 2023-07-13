092504a75842b9a01861c5a6f9b3c6c01ae95b76
Rohith Vemuri <137885748+RohithV2001@users.noreply.github.com>
2023-07-13 14:43:11 +0530

    Add files via upload

---
+++ Sorting/Insertion Sort.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def insertionSort(arr: List[int], n: int) -> None:
+    for i in range(1,n):
+        key=arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+            arr[j+1]=arr[j]
+            j=j-1
+        arr[j+1]=key
+    return arr 
