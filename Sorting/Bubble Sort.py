76f329bf217010bc01d8571ebc03294bfcc6bae1
Rohith Vemuri <137885748+RohithV2001@users.noreply.github.com>
2023-07-13 14:33:47 +0530

    Add files via upload

---
+++ Sorting/Bubble Sort.py
@@ -0,0 +1,6 @@
+def bubbleSort(arr: [int], n: int):
+    for i in range(len(arr)):
+        for j in range(len(arr)-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
