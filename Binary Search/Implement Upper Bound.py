0ab488b70cb647d671543a687a37587195cad5c1
Rohith Vemuri <137885748+RohithV2001@users.noreply.github.com>
2023-07-20 19:13:59 +0530

    Add files via upload

---
+++ Binary Search/Implement Upper Bound.py
@@ -0,0 +1,12 @@
+def upperBound(arr: [int], x: int, n: int) -> int:
+    low=0
+    high=n-1
+    ans=n
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid]>x:
+            ans=mid
+            high=mid-1
+        else:
+            low=mid+1
+    return ans
