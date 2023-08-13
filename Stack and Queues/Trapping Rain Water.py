817e0ba04b23c4351a2f91155790525c7953b4ea
Rohith Vemuri <137885748+RohithV2001@users.noreply.github.com>
2023-08-13 14:39:33 +0530

    Add files via upload

---
+++ Stack and Queues/Trapping Rain Water.py
@@ -0,0 +1,21 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        lm=0
+        rm=0
+        l=0
+        r=len(height)-1
+        res=0
+        while l<r:
+            if height[l]<=height[r]:
+                if height[l]>=lm:
+                    lm=height[l]
+                else:
+                    res=res+lm-height[l]
+                l=l+1
+            else:
+                if height[r]>=rm:
+                    rm=height[r]
+                else:
+                    res=res+rm-height[r]
+                r=r-1
+        return re
