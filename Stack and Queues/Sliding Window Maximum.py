13d3918cd23dd2de13d91bf1fa3f7f97bcb2f7a0
Rohith Vemuri <137885748+RohithV2001@users.noreply.github.com>
2023-08-14 15:53:17 +0530

    Add files via upload

---
+++ Stack and Queues/Sliding Window Maximum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if len(nums)==k:
+             return [max(nums)]
+        i,j=0,0
+        l=[]
+        ans=[]
+        while j<len(nums):
+            while len(l)>0 and l[-1]<nums[j]:
+                l.pop()
+            l.append(nums[j])
+            if j-i+1<k:
+                j+=1
+            elif j-i+1==k:
+                ans.append(l[0])
+                if l[0]==nums[i]:
+                    l.pop(0)
+                i+=1
+                j+=1
+        return ans
