b36c743f3e5134d2364b049bec98fddb3dd7e8a7
Rohith Vemuri <137885748+RohithV2001@users.noreply.github.com>
2023-08-15 15:50:12 +0530

    Add files via upload

---
+++ Sliding Window and Two Pointer/Max Consecutive Ones III.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        mx=0
+        l=0
+        for r,num in enumerate(nums):
+            k-=1-num
+            if k<0:
+                k+=1-nums[l]
+                l+=1
+            else:
+                mx=max(mx,r-l+1)
+        return mx
